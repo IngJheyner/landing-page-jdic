dcfcfd716b05dbb557db3d9995cad4b09b372a34
IngJheyner <david-ibagon@hotmail.com>
2025-02-19 17:55:43 -0500

    Se configura vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,20 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue(), vueform()],
   base: "/",
+  build: {
+    outDir: 'dist', // Directorio de salida
+    assetsDir: 'assets', // Directorio para assets
+    chunkSizeWarningLimit: 1500, // Aumenta el límite de tamaño del chunk
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
